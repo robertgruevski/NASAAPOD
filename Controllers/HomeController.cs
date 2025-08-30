0290d4a9e7c787a559a9f455050a7042097590de
robertgruevski <robertgruevski@outlook.com>
2025-08-30 16:37:02 -0400

    Get apod in Home Index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,50 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using NASAAPOD.Models;
 
 namespace NASAAPOD.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private const string NASA_API_KEY = "DEMO_KEY"; // Replace with your actual API key
+		private const string NASA_API_URL = "https://api.nasa.gov/planetary/apod";
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public async Task<IActionResult> Index()
+		{
+			var apodData = new Apod();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+			try
+			{
+				var httpClient = _httpClientFactory.CreateClient();
+				var response = await httpClient.GetAsync($"{NASA_API_URL}?api_key={NASA_API_KEY}");
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+				if (response.IsSuccessStatusCode)
+				{
+					var jsonResponseString = await response.Content.ReadAsStringAsync();
+					apodData = JsonSerializer.Deserialize<Apod>(jsonResponseString, new JsonSerializerOptions
+					{
+						PropertyNameCaseInsensitive = true
+					});
+				}
+				else
+				{
+					ViewBag.ErrorMessage = "Failed to retrieve data from NASA API.";
+				}
+			}
+			catch (Exception ex)
+			{
+				ViewBag.ErrorMessage = $"An error occurred while fetching data. {ex.Message}";
+			}
+
+			return View(apodData);
+		}
+	}
 }
