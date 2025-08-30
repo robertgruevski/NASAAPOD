ddd7eb7f6f1c8a59a095e6e6cf40255c5ea71df2
robertgruevski <robertgruevski@outlook.com>
2025-08-30 16:29:31 -0400

    Model setup

---
+++ Models/Apod.cs
@@ -0,0 +1,12 @@
+﻿namespace NASAAPOD.Models
+{
+	public class Apod
+	{
+		public string? Title { get; set; }
+		public string? Explanation { get; set; }
+		public string? Url { get; set; }
+		public string? Media_type { get; set; }
+		public string? Hdurl { get; set; }
+		public string? Copyright { get; set; }
+	}
+}
